5e8db3e70e9ef027d05d96db340e32e114a08a81
Snubis6 <snubis9@gmail.com>
2021-11-18 05:11:01 +0100

    99

--- myScript.js
+++ myScript.js
@@ -28,3 +28,18 @@ function myFunction2() {
       moreText.style.display = "inline";
     }
 }
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
