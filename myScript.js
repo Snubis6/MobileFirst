96ffbcb2a06d5ad9564f5d482aaa587e20f998d7
Snubis6 <snubis9@gmail.com>
2021-11-19 11:28:24 +0100

    did stuff

--- myScript.js
+++ myScript.js
@@ -44,17 +44,3 @@ function four() {
   }
 }
 
-// Get the elements with class="column"
-var elements = document.getElementsByClassName("newsgrid");
-
-// Declare a loop variable
-var i;
-
-
-// Four images side by side
-function four() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "25%";  // IE10
-    elements[i].style.flex = "25%";
-  }
-}
