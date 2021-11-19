0b5dd2877e2d4bb00605a340c84dfd6e367e8e12
Snubis6 <snubis9@gmail.com>
2021-11-19 11:09:27 +0100

    news thngy

--- myScript.js
+++ myScript.js
@@ -43,3 +43,18 @@ function four() {
     elements[i].style.flex = "25%";
   }
 }
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("newsgrid");
+
+// Declare a loop variable
+var i;
+
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
